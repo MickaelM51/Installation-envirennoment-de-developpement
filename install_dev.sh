13500be19ea15ba22d7ed2b4524cf30b74c4441d
Mickael M <43402575+MickaelM51@users.noreply.github.com>
2019-07-25 11:09:22 +0200

    modif sudo php.phar

--- install_dev.sh
+++ install_dev.sh
@@ -116,7 +116,7 @@ fi
 
 #Fin installation phpMyAdmin
 
-# Debut installation de composer ! A REVOIR POUR LES BUGS !!
+# Debut installation de composer 
 
 echo -n " Voulez vous installer Composer ? y/n :"
 read ouinoncomposer
@@ -125,7 +125,7 @@ if [ $ouinoncomposer = "y" ] || [ $ouinoncomposer = "Y" ]; then
     php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
     php composer-setup.php
     php -r "unlink('composer-setup.php');"
-    mv composer.phar /usr/local/bin/composer
+    sudo mv composer.phar /usr/local/bin/composer
     echo "
             ------------------------
             | Composer installer |
